2ea6f2beb3264f0497fcf58fc3f66012a170d0a2
mertyuceyurt <mertyuceyurt58@gmail.com>
2025-10-20 22:11:02 +0300

    pass project name in gee init for colab

--- src/gee/base_gee.py
+++ src/gee/base_gee.py
@@ -3,7 +3,7 @@ import sys
 
 def init():
     try:
-        ee.Initialize()
+        ee.Initialize(project='agro-estimation-project')
         print("GEE kimlik doğrulaması başarılı.")
     except Exception as e:
         print(f"GEE başlatılamadı. Hata: {e}", file=sys.stderr)
