b22d9d20349b34bcdb7a8531ecedafff2669cc45
mertyuceyurt <mertyuceyurt58@gmail.com>
2025-11-04 21:19:11 +0300

    use sentinel-2 sattelite for ndvi after 2015

--- src/gee/collect_point_data.py
+++ src/gee/collect_point_data.py
@@ -1,29 +1,44 @@
 import os
 import ee
 import pandas as pd
+from datetime import datetime
 from . import base_gee
+# import base_gee
 
-def collect_point_data(lon, lat, date_start='2024-05-08', date_end='2024-06-08', output_dir="data/processed"):
+def collect_point_data(lon, lat, date_start='2006-05-08', date_end='2006-06-08', output_dir="data/processed"):
 
     base_gee.init()
 
     point = ee.Geometry.Point([lon, lat])
 
-    # --- NDVI (MODIS) ---
-    modis = (
-        ee.ImageCollection('MODIS/061/MOD13Q1') #TODO: MODIS yerine Sentinel-2 kullanabiliriz
-        .filterBounds(point)
-        .filterDate(date_start, date_end)
-        .select('NDVI')
-        .map(lambda img: img.multiply(0.0001).copyProperties(img, ['system:time_start']))
-    )
+    # --- NDVI (SENTINEL 2 for >2015, MODIS for <2015 ) ---
 
-    ndvi_fc = modis.map(lambda img: ee.Feature(None, {
+    start_year = datetime.strptime(date_start, "%Y-%m-%d").year
+    if start_year < 2015:
+        ndvi_collection = (
+            ee.ImageCollection('MODIS/061/MOD13Q1')
+            .filterBounds(point)
+            .filterDate(date_start, date_end)
+            .select('NDVI')
+            .map(lambda img: img.multiply(0.0001).copyProperties(img, ['system:time_start']))
+        )
+    else:
+        ndvi_collection = (
+            ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+            .filterBounds(point)
+            .filterDate(date_start, date_end)
+            .map(lambda img: img.addBands(
+                img.normalizedDifference(['B8', 'B4']).rename('NDVI')
+            ).copyProperties(img, ['system:time_start']))
+            .select('NDVI')
+        )
+
+    ndvi_fc = ndvi_collection.map(lambda img: ee.Feature(None, {
         'date': ee.Date(img.get('system:time_start')).format('YYYY-MM-dd'),
         'NDVI': img.reduceRegion(ee.Reducer.mean(), point, 250).get('NDVI')
     }))
 
-    print(f"NDVI görüntü sayısı: {modis.size().getInfo()}")
+    print(f"NDVI görüntü sayısı: {ndvi_collection.size().getInfo()}")
 
     # --- Yağış (CHIRPS) ---
     chirps = (
@@ -60,16 +75,27 @@ def collect_point_data(lon, lat, date_start='2024-05-08', date_end='2024-06-08',
 
     # --- EE FeatureCollection -> Pandas DataFrame ---
     def fc_to_df(fc, limit=5000):
-        features = fc.toList(limit)
-        data = []
-        for i in range(limit):
-            try:
-                props = features.get(i).getInfo()['properties']
-                data.append(props)
-                print(f"İşlenen özellik {i+1}: {props}")
-            except Exception:
-                break
-        return pd.DataFrame(data)
+            fc_size = fc.size().getInfo()
+            print(f"Özellik koleksiyonu boyutu: {fc_size}")
+
+            num_to_process = min(fc_size, limit)
+            
+            if num_to_process == 0:
+                print("Uyarı: Koleksiyon boş. Boş DataFrame döndürülüyor.")
+                return pd.DataFrame()
+
+            features = fc.toList(num_to_process)
+            
+            data = []
+            for i in range(num_to_process):
+                try:
+                    props = features.get(i).getInfo()['properties']
+                    print(f"İşlenen özellik {i+1}/{num_to_process}: {props}")
+                    data.append(props)
+                except Exception as e:
+                    print(f"Özellik {i} işlenirken HATA oluştu: {e}")
+            
+            return pd.DataFrame(data)
 
     ndvi_df = fc_to_df(ndvi_fc)
     rain_df = fc_to_df(rain_fc)
